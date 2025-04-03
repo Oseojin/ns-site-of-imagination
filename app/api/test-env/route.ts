5b593abc757d5dff00f874b27d32dfde359f3517
Oseojin <oseojin5137@gmail.com>
2025-04-03 12:05:37 +0900

    change test code

--- app/api/test-env/route.ts
+++ app/api/test-env/route.ts
@@ -2,9 +2,9 @@
 export async function GET() {
   console.log(process.env);
   return Response.json({
-    NEXTAUTH_URL: process.env["NEXTAUTH_URL"],
-    GOOGLE_CLIENT_ID: process.env["GOOGLE_CLIENT_ID"],
-    KAKAO_CLIENT_ID: process.env["KAKAO_CLIENT_ID"],
-    DISCORD_CLIENT_ID: process.env["DISCORD_CLIENT_ID"],
+    NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+    GOOGLE_CLIENT_ID: process.env["NEXT_PUBLIC_GOOGLE_CLIENT_ID"],
+    KAKAO_CLIENT_ID: process.env["NEXT_PUBLIC_KAKAO_CLIENT_ID"],
+    DISCORD_CLIENT_ID: process.env["NEXT_PUBLIC_DISCORD_CLIENT_ID"],
   });
 }
