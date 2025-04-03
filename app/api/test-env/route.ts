777364f85944e34d3267a9c3e4e4254243c661b0
Oseojin <oseojin5137@gmail.com>
2025-04-03 11:56:52 +0900

    add test console

--- app/api/test-env/route.ts
+++ app/api/test-env/route.ts
@@ -1,5 +1,6 @@
 // app/api/test-env/route.ts
 export async function GET() {
+  console.log(process.env);
   return Response.json({
     NEXTAUTH_URL: process.env["NEXTAUTH_URL"],
     GOOGLE_CLIENT_ID: process.env["GOOGLE_CLIENT_ID"],
