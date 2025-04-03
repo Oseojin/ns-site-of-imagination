c7c51a1ee4265777296c18ff30cdff33d2d39ec9
Oseojin <oseojin5137@gmail.com>
2025-04-03 11:43:37 +0900

    env test

---
+++ app/api/test-env/route.ts
@@ -0,0 +1,9 @@
+// app/api/test-env/route.ts
+export async function GET() {
+  return Response.json({
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    KAKAO_CLIENT_ID: process.env.KAKAO_CLIENT_ID,
+    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+  });
+}
