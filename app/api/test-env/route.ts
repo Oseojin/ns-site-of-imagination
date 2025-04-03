16d8b0abaa9e1cf3127f1fd35ab86bf9cfc03048
Oseojin <oseojin5137@gmail.com>
2025-04-03 12:23:50 +0900

    testChange v3

--- app/api/test-env/route.ts
+++ app/api/test-env/route.ts
@@ -3,8 +3,8 @@ export async function GET() {
   console.log(process.env);
   return Response.json({
     NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
-    GOOGLE_CLIENT_ID: process.env["NEXT_PUBLIC_GOOGLE_CLIENT_ID"],
-    KAKAO_CLIENT_ID: process.env["NEXT_PUBLIC_KAKAO_CLIENT_ID"],
-    DISCORD_CLIENT_ID: process.env["NEXT_PUBLIC_DISCORD_CLIENT_ID"],
+    GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    KAKAO_CLIENT_ID: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID,
+    DISCORD_CLIENT_ID: process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID,
   });
 }
