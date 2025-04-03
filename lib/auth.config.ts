39541240deecbf8b8c58c1487a62cde607a0c09e
Oseojin <oseojin5137@gmail.com>
2025-04-03 12:16:38 +0900

    change route PUBLIC

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -7,15 +7,15 @@ import KakaoProvider from "next-auth/providers/kakao";
 export const authConfig: AuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID!,
       clientSecret: process.env.KAKAO_CLIENT_SECRET!,
     }),
     DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientId: process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID!,
       clientSecret: process.env.DISCORD_CLIENT_SECRET!,
     }),
   ],
