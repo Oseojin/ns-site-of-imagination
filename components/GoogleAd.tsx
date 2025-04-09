3399d09b5d7c8766137566c49bc4596c62d8a058
Oseojin <oseojin5137@gmail.com>
2025-04-10 08:33:57 +0900

    modify googld ad

--- components/GoogleAd.tsx
+++ components/GoogleAd.tsx
@@ -1,11 +1,17 @@
+// app/GoogleAd.tsx
+"use client";
+
+import { useEffect } from "react";
+
 export default function GoogleAd() {
-  return (
-    <div>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1291810764124876"
-        crossOrigin="anonymous"
-      ></script>
-    </div>
-  );
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1291810764124876";
+    script.async = true;
+    script.crossOrigin = "anonymous";
+    document.body.appendChild(script);
+  }, []);
+
+  return <div id="google-ad" />;
 }
