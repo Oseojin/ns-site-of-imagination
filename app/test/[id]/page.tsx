9d95f70c2ecd4d40d4a5eb0f71e72359b96ce2a3
Oseojin <oseojin5137@gmail.com>
2025-04-13 21:53:13 +0900

    remove console.log

--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -21,7 +21,6 @@ export default function TestIntroPage() {
       }
 
       const data = await res.json();
-      console.log(data);
       const parsed: TestPayload = {
         title: data.title,
         titleImage: data.titleImage,
